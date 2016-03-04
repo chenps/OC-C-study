13bd31d2d56201cdb3ae9a4d9ceaa8fd5304be37
Patience <chenps10@gmail.com>
2016-03-04 15:57:45 +0800

    Objetive-c和c语言基础学习第一次上传

---
+++ Ctype/Ctype/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ctype
+//
+//  Created by 陈培升 on 16/3/4.
+//  Copyright © 2016年 陈培升. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
